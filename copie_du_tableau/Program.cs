e657f1174858c21d5a0489b7483af560b340bf24
CaePolit <caepolit@gmail.com>
2024-06-11 09:51:57 +0200

    Ajoutez des fichiers projet.

---
+++ copie_du_tableau/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("taille de petits tableau : ");
+        int taille = int.Parse(Console.ReadLine());
+        int[] tab = new int[taille];
+        int[] tab2 = new int[taille];
+        for (int i = 0; i < taille; i++)
+        {
+            tab[i] = i;
+            tab2[i] = 2 * i;
+        }
+        int[] tab3 = tab.Concat(tab2).ToArray();
+        for (int i = 0; i < tab.Length; i++)
+        {
+            Console.WriteLine("premier tableau : " + tab[i]);
+        }
+        for (int i = 0; i < tab2.Length; i++)
+        {
+            Console.WriteLine("deuxieme tableau : " + tab2[i]);
+        }
+        for (int i = 0; i < tab3.Length; i++)
+        {
+            Console.WriteLine("troisieme tableau : " + tab3[i]);
+        }
+
+    }
+}
